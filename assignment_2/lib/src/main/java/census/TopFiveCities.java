9c22401a1f8addefd95e56d05a8cbea168c4b0ca
Kaitlyn Asato <kaitlyn.asato.22@cnu.edu>
2024-03-26 14:39:21 -0400

    working on update and topFive

--- assignment_2/lib/src/main/java/census/TopFiveCities.java
+++ assignment_2/lib/src/main/java/census/TopFiveCities.java
@@ -1,10 +1,43 @@
 package census;
 
-public class TopFiveCities implements Observer{
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class TopFiveCities implements Observer{
+	
+	private List<City> cityList = new ArrayList<>();
+
+	private String name;
+	private String state;
+	private int population;
+	
 	@Override
 	public void update(Observable ob) {
-		// TODO Auto-generated method stub
+		if (ob instanceof CensusOffice office) {
+			this.name = office.getReported().getName();
+			this.state = office.getReported().getState();
+			this.population = office.getReported().getPopulation();
+			
+			City currCity = new City(name, state, population);
+			
+			cityList.add(currCity);
+		}
+		
+	}
+	
+	public List<City> getTopFive(){
+		
+		List<City> topList = new ArrayList<>();;
+		
+		cityList.sort(Comparator.comparingInt(City::getPopulation));
+		
+		for(int i = 0; i < 5; i++ ) {
+			topList.add(cityList.get(i));
+		}
+		
+		
+		return topList;
 		
 	}
 
