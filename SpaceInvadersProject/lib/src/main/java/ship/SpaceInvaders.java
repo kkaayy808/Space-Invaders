ce594ccd6864c1186093aa348742e3279c46e221
Kaitlyn Asato <kaitlyn.asato.22@cnu.edu>
2024-04-12 20:03:51 -0400

    set up JFrame spaceInvaders class

--- SpaceInvadersProject/lib/src/main/java/ship/SpaceInvaders.java
+++ SpaceInvadersProject/lib/src/main/java/ship/SpaceInvaders.java
@@ -1,7 +1,57 @@
 package ship;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 
+@SuppressWarnings("serial")
 public class SpaceInvaders extends JFrame{
-
+	
+	public SpaceInvaders() {
+		setTitle("Space Invaders");
+		setSize(500, 450);
+		
+		var menu = new JMenuBar();
+		setJMenuBar(menu);
+		
+		var game = new JMenu("Game");
+		var help = new JMenu("Help");
+		
+		
+		//"Game" menu drop down
+		menu.add(game);
+		var newGame = game.add("New Game");
+		game.addSeparator();
+		var pause = game.add("Pause");
+		var resume = game.add("Resume");
+		game.addSeparator();
+		var quit = game.add("Quit");
+		
+		quit.addActionListener(e -> {
+			var result = JOptionPane.showConfirmDialog(SpaceInvaders.this, "Dare to Quit?");
+			if(result == JOptionPane.YES_OPTION) {
+				dispose();
+			}
+		});
+		
+		
+		//"Help" menu drop down
+		menu.add(help);
+		var about = help.add("About...");
+		
+		about.addActionListener(e -> JOptionPane.showMessageDialog(SpaceInvaders.this, 
+				new JLabel("<html><b>SpaceInvaders</b><br>by Josiah Briggs & Kaitlyn Asato<hr><")));
+		
+		
+		
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		var f = new SpaceInvaders();
+		f.setVisible(true);
+	}
 }
