4d076cd06f13f62d884d29a289621b4093cbbabe
Josiah Briggs <josiah.briggs.22@cnu.edu>
2024-04-18 10:22:04 -0400

    initial missle class

--- SpaceInvadersProject/lib/src/main/java/ship/Missile.java
+++ SpaceInvadersProject/lib/src/main/java/ship/Missile.java
@@ -1,5 +1,28 @@
 package ship;
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.sound.sampled.Clip;
 
 public class Missile extends Drawable{
-
+		private Image image;
+		private Clip  sound;
+		
+		public Missile(int x, int y) {
+			super( x, y );
+			image = getImage( "photon.png" ); // wrong image
+			sound = getSound( "aud_basefire.wav" );
+		}
+		@Override
+		public void draw(Graphics2D g2) {
+			g2.drawImage( image, getX(), getY(), 50, 50, null );
+		}
+		public void move() {
+			setY( getY() - 8 );
+		}
+		public void playSound() {
+			sound.setFramePosition(0);
+			sound.start();
+		}
+	}
 }
