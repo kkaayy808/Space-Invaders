275ec167189da4720a57060664e71e2d54e1b933
Kaitlyn Asato <kaitlyn.asato.22@cnu.edu>
2024-04-17 23:12:47 -0400

    added drawable class methods

--- SpaceInvadersProject/lib/src/main/java/ship/Drawable.java
+++ SpaceInvadersProject/lib/src/main/java/ship/Drawable.java
@@ -1,5 +1,66 @@
 package ship;
 
-public abstract class Drawable {
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+
+public abstract class Drawable {
+	
+	private int x;
+	private int y;
+
+	public Drawable(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public abstract void draw(Graphics2D g2);
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	protected Image getImage(String filename) {
+		URL url = getClass().getResource(filename);
+		ImageIcon icon = new ImageIcon(url);
+		return icon.getImage();
+	}
+	
+	protected Clip getSound(String filename) {
+		Clip clip = null;
+		try {
+			InputStream in = getClass().getResourceAsStream(filename);
+			InputStream buf = new BufferedInputStream(in);
+			AudioInputStream stream = AudioSystem.getAudioInputStream(buf);
+			clip = AudioSystem.getClip();
+			clip.open(stream); 
+		} catch (UnsupportedAudioFileException | 
+				IOException | LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		return clip;
+	}
+	
 }
