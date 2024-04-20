6e52e36114d2431eea5bb59228cd2fbdd7227eb7
Josiah Briggs <josiah.briggs.22@cnu.edu>
2024-04-20 17:51:21 -0400

    basic panel

--- SpaceInvadersProject/lib/src/main/java/ship/Panel.java
+++ SpaceInvadersProject/lib/src/main/java/ship/Panel.java
@@ -1,7 +1,43 @@
 package ship;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Panel extends JPanel{
-
+	
+	public Panel() {
+		setPreferredSize(new Dimension(500, 400));
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+            }
+        });
+		
+		Timer timer = new Timer(20, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				repaint();
+			}
+		});
+		timer.start();
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+	}
 }
