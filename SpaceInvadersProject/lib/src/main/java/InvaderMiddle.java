e3e4be69c3d8a947ad577abb773fdc3d28803ada
Josiah Briggs <josiah.briggs.22@cnu.edu>
2024-04-22 19:56:47 -0400

    working on invaders

--- SpaceInvadersProject/lib/src/main/java/InvaderMiddle.java
+++ SpaceInvadersProject/lib/src/main/java/InvaderMiddle.java
@@ -1,5 +1,15 @@
-
+import java.awt.Color;
+import java.awt.Graphics2D;
 
 public class InvaderMiddle extends Invader{
 
+	public InvaderMiddle(int x, int y) {
+		super(x, y);
+		points = 20;
+	}
+	
+	@Override
+    public void draw(Graphics2D g2) {
+        //add image
+    }
 }
--- SpaceInvadersProject/lib/src/main/java/InvaderTop.java
+++ SpaceInvadersProject/lib/src/main/java/InvaderTop.java
@@ -2,4 +2,14 @@
 
 public class InvaderTop extends Invader{
 
+	public InvaderTop(int x, int y) {
+		super(x, y);
+		points = 30;
+	}
+	
+	@Override
+	public void draw(Graphics2D g2) {
+		//add image
+	}
+
 }
